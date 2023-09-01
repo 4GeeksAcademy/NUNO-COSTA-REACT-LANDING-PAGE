4465a5e6d370eba1a2d4803e52a1f5341263193a
Broasdemilho <135333779+Broasdemilho@users.noreply.github.com>
2023-09-01 18:58:48 +0000

    added cards and footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar";
+import Card from "./card";
+import Jumbotron from "./Jumbotron";
+import Footer from "./footer";
+import { element } from "prop-types";
+
+const arrayOfCards = [
+	{
+	imgUrl: "https://pngimg.com/uploads/pirate/pirate_PNG39.png",
+	title: "Hello",
+	description: "Hi",
+	buttonUrl: "",
+	buttonLabel: "Cool",
+},
+{
+	imgUrl: "https://kidsclubforjesus.org/img/nature/facts/crocodile-full-img.jpg",
+	title: "go",
+	description: "hey",
+	buttonUrl: "",
+	buttonLabel: "sup",
+},
+{
+	imgUrl: "https://farm66.static.flickr.com/65535/52949770368_e12f876b6a.jpg",
+	title: "nice",
+	description: "world",
+	buttonUrl: "",
+	buttonLabel: "helicopter",
+}];
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div>
+			<Navbar/>
 		</div>
+		<div>
+			<Jumbotron/>
+		</div>
+		<div className="d-flex container">	
+		{arrayOfCards.map((element, i)=> {
+			return(
+				<Card imgUrl={element.imgUrl}
+				title={element.title}
+				description={element.description}
+				buttonUrl={element.buttonUrl}
+				buttonLabel={element.buttonLabel} />
+			)
+		} )}
+		</div>
+			<Footer className="mb-0"/>
+		</>
 	);
 };
 
+
+
 export default Home;
+
